d25f5f13e96bc2ff666d348a9ac7467a56a89a67
JoaquimFontesMatos <joaquimatos50@gmail.com>
2025-10-07 16:21:36 +0000

    fix

--- config/releases.exs
+++
@@ -1,5 +0,0 @@
-import Config
-
-config :gymrat, GymratWeb.Endpoint,
-  server: true,
-  cache_static_manifest: "priv/static/cache_manifest.json"
--- config/runtime.exs
+++ config/runtime.exs
@@ -17,7 +17,9 @@ import Config
 # Alternatively, you can use `mix phx.gen.release` to generate a `bin/server`
 # script that automatically sets the env var above.
 if System.get_env("PHX_SERVER") do
-  config :gymrat, GymratWeb.Endpoint, server: true
+  config :gymrat, GymratWeb.Endpoint,
+    server: true,
+    cache_static_manifest: "priv/static/cache_manifest.json"
 end
 
 if config_env() == :prod do
