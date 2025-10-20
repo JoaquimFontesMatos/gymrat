b84fa8be3043f07e7d8a9eecf9b5d9e626304e6c
JoaquimFontesMatos <joaquimatos50@gmail.com>
2025-10-20 18:18:34 +0000

    changed db again

--- config/runtime.exs
+++ config/runtime.exs
@@ -50,14 +50,12 @@ if config_env() == :prod do
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "2"),
     # For machines with several cores, consider starting multiple pools of `pool_size`
     pool_count: 4,
-    # ,
     socket_options: maybe_ipv6,
-    ssl: true
-
-  # ssl: [
-  #   verify: :verify_peer,
-  #   cacertfile: ca_path
-  # ]
+    # ssl: true
+    ssl: [
+      verify: :verify_peer,
+      cacertfile: ca_path
+    ]
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
