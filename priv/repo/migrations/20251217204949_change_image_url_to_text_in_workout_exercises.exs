330b00bf42fcc3831001a0767df6dc6e7d446589
JoaquimFontesMatos <joaquimatos50@gmail.com>
2025-12-17 22:14:55 +0000

    fix: fixed last migration

--- priv/repo/migrations/20251217204949_change_image_url_to_text_in_workout_exercises.exs
+++ priv/repo/migrations/20251217204949_change_image_url_to_text_in_workout_exercises.exs
@@ -1,19 +1,15 @@
-defmodule Gymrat.Repo.Migrations.AddSupportForCustomExercise do
+defmodule Gymrat.Repo.Migrations.ChangeImageUrlToTextInWorkoutExercises do
   use Ecto.Migration
 
-  defmodule Gymrat.Repo.Migrations.AddSupportForCustomExercise do
-    use Ecto.Migration
-
-    def up do
-      alter table(:workout_exercises) do
-        modify :custom_image_url, :text
-      end
+  def up do
+    alter table(:workout_exercises) do
+      modify :custom_image_url, :text
     end
+  end
 
-    def down do
-      alter table(:workout_exercises) do
-        remove :custom_image_url, :string
-      end
+  def down do
+    alter table(:workout_exercises) do
+      remove :custom_image_url, :string
     end
   end
 end
