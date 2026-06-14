e05111b91e0095ca3475479292fa77e9be41bccc
JoaquimFontesMatos <joaquimatos50@gmail.com>
2026-06-14 21:06:46 +0100

    feat: Added share, edit, and delete buttons for plan details

--- lib/gymrat_web/live/plan_live/details.ex
+++ lib/gymrat_web/live/plan_live/details.ex
@@ -34,10 +34,86 @@ defmodule GymratWeb.PlanLive.Details do
   def render(assigns) do
     ~H"""
     <Layouts.app flash={@flash} current_scope={@current_scope}>
-      <.header_with_back_navigate navigate={~p"/"} title={@plan.name} />
-      <h2 class="font-bold text-lg">Workouts</h2>
+      <div class="flex justify-between items-center gap-2">
+        <.header_with_back_navigate navigate={~p"/"} title={@plan.name} />
+        <div class="flex flex-wrap justify-end gap-2" phx-hook="Share" id="plan-actions">
+          <.button
+            class="btn btn-primary btn-soft btn-square"
+            phx-click={
+              JS.dispatch("share-plan",
+                detail: %{share_token: @plan.share_token, name: @plan.name},
+                to: "#plan-actions"
+              )
+            }
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              class="size-6"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M15.75 4.5a3 3 0 1 1 .825 2.066l-8.421 4.679a3.002 3.002 0 0 1 0 1.51l8.421 4.679a3 3 0 1 1-.729 1.31l-8.421-4.678a3 3 0 1 1 0-4.132l8.421-4.679a3 3 0 0 1-.096-.755Z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </.button>
 
+          <.button
+            :if={@current_user_id == @plan.creator_id}
+            phx-click="update_plan"
+            class="btn btn-primary btn-soft btn-square"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              class="size-[1.2em]"
+            >
+              <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32L19.513 8.2Z" />
+            </svg>
+          </.button>
+
+          <.button
+            phx-click="show_modal"
+            class="btn btn-error btn-soft btn-square"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              class="size-[1.2em]"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </.button>
+
+          <.modal
+            :if={@show_modal}
+            id="confirm-modal"
+            on_cancel={JS.push("hide_modal")}
+          >
+            <h2>Are you sure you want to delete this plan?</h2>
+            <p>This action cannot be undone.</p>
+            <div class="modal-action">
+              <.button phx-click="hide_modal">
+                Cancel
+              </.button>
+              <.button class="btn btn-error" phx-click="delete_plan">
+                Confirm
+              </.button>
+            </div>
+          </.modal>
+        </div>
+      </div>
       <ul>
+        <h2 class="font-bold text-lg">Workouts</h2>
+
         <%= for workout <- @plan.workouts do %>
           <.list_item navigate={~p"/plans/#{workout.plan_id}/workouts/#{workout.id}"}>
             <.workout_icon name={resolve_icon(workout)} class="w-9 h-12 text-primary shrink-0" />
@@ -71,81 +147,6 @@ defmodule GymratWeb.PlanLive.Details do
           </.button>
         <% end %>
       </ul>
-
-      <div class="flex flex-wrap justify-end gap-2" phx-hook="Share" id="plan-actions">
-        <.button
-          class="btn btn-primary btn-soft btn-square"
-          phx-click={
-            JS.dispatch("share-plan",
-              detail: %{share_token: @plan.share_token, name: @plan.name},
-              to: "#plan-actions"
-            )
-          }
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            class="size-6"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M15.75 4.5a3 3 0 1 1 .825 2.066l-8.421 4.679a3.002 3.002 0 0 1 0 1.51l8.421 4.679a3 3 0 1 1-.729 1.31l-8.421-4.678a3 3 0 1 1 0-4.132l8.421-4.679a3 3 0 0 1-.096-.755Z"
-              clip-rule="evenodd"
-            />
-          </svg>
-        </.button>
-
-        <.button
-          :if={@current_user_id == @plan.creator_id}
-          phx-click="update_plan"
-          class="btn btn-primary btn-soft btn-square"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            class="size-[1.2em]"
-          >
-            <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32L19.513 8.2Z" />
-          </svg>
-        </.button>
-
-        <.button
-          phx-click="show_modal"
-          class="btn btn-error btn-soft btn-square"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            class="size-[1.2em]"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z"
-              clip-rule="evenodd"
-            />
-          </svg>
-        </.button>
-
-        <.modal
-          :if={@show_modal}
-          id="confirm-modal"
-          on_cancel={JS.push("hide_modal")}
-        >
-          <h2>Are you sure you want to delete this plan?</h2>
-          <p>This action cannot be undone.</p>
-          <div class="modal-action">
-            <.button phx-click="hide_modal">
-              Cancel
-            </.button>
-            <.button class="btn btn-error" phx-click="delete_plan">
-              Confirm
-            </.button>
-          </div>
-        </.modal>
-      </div>
     </Layouts.app>
     """
   end
--- lib/gymrat_web/live/workout_live/details.ex
+++ lib/gymrat_web/live/workout_live/details.ex
@@ -33,8 +33,62 @@ defmodule GymratWeb.WorkoutLive.Details do
   def render(assigns) do
     ~H"""
     <Layouts.app flash={@flash} current_scope={@current_scope}>
-      <.header_with_back_navigate navigate={~p"/plans/#{@plan_id}"} title={@workout.name} />
+      <div class="flex justify-between items-center gap-2">
+        <.header_with_back_navigate navigate={~p"/plans/#{@plan_id}"} title={@workout.name} />
 
+        <div class="flex flex-wrap justify-end gap-2">
+          <.button
+            :if={@is_workout_owner}
+            phx-click="update_workout"
+            class="btn btn-primary btn-soft btn-square"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              class="size-[1.2em]"
+            >
+              <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32L19.513 8.2Z" />
+            </svg>
+          </.button>
+
+          <.button
+            :if={@is_workout_owner}
+            class="btn btn-error btn-soft btn-square"
+            phx-click="show_modal"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              class="size-[1.2em]"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </.button>
+
+          <.modal
+            :if={@show_modal}
+            id="confirm-modal"
+            on_cancel={JS.push("hide_modal")}
+          >
+            <h2>Are you sure you want to delete this workout?</h2>
+            <p>This action cannot be undone.</p>
+            <div class="modal-action">
+              <.button phx-click="hide_modal">
+                Cancel
+              </.button>
+              <.button class="btn btn-error" phx-click="delete_workout">
+                Confirm
+              </.button>
+            </div>
+          </.modal>
+        </div>
+      </div>
       <div class="flex items-center gap-2">
         <.workout_icon name={resolve_icon(@workout)} class="w-14 h-20 text-primary" />
 
@@ -92,59 +146,6 @@ defmodule GymratWeb.WorkoutLive.Details do
           </.button>
         <% end %>
       </ul>
-
-      <div class="flex flex-wrap justify-end gap-2">
-        <.button
-          :if={@is_workout_owner}
-          phx-click="update_workout"
-          class="btn btn-primary btn-soft btn-square"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            class="size-[1.2em]"
-          >
-            <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32L19.513 8.2Z" />
-          </svg>
-        </.button>
-
-        <.button
-          :if={@is_workout_owner}
-          class="btn btn-error btn-soft btn-square"
-          phx-click="show_modal"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            class="size-[1.2em]"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z"
-              clip-rule="evenodd"
-            />
-          </svg>
-        </.button>
-
-        <.modal
-          :if={@show_modal}
-          id="confirm-modal"
-          on_cancel={JS.push("hide_modal")}
-        >
-          <h2>Are you sure you want to delete this workout?</h2>
-          <p>This action cannot be undone.</p>
-          <div class="modal-action">
-            <.button phx-click="hide_modal">
-              Cancel
-            </.button>
-            <.button class="btn btn-error" phx-click="delete_workout">
-              Confirm
-            </.button>
-          </div>
-        </.modal>
-      </div>
     </Layouts.app>
     """
   end
